a49c012e335f636e88e10f309f8a5798e98a3479
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-01-12 21:05:29 +0100

    tasks 3 completed

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 -- as their main style, ranked by their longevity
 SELECT band_name, IFNULL(split - formed, 2020 - formed) lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;
