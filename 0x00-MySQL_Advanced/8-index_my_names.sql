62b9a219541d979e72d7b2b9cbcd5d3fd748aa8f
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-01-14 00:27:49 +0100

    task 8 completed

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,4 @@
+-- This is a MySQL script  that creates an index idx_name_first
+-- on the table names and the first letter of name
+DROP INDEX idx_name_first ON names;
+CREATE INDEX idx_name_first ON names (name(1));
