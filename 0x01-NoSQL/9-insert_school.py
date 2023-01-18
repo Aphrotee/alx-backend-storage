6022141af4deaf75d14b49f1378a55909bf32bc2
Aiyebogan Temitope <aphrotemitope37@gmail.com>
2023-01-18 15:47:12 +0100

    edited task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -12,4 +12,5 @@ def insert_school(mongo_collection, **kwargs):
     """
     if mongo_collection is None or kwargs == {} or kwargs is None:
         return
-    return mongo_collection.insert_one(kwargs)
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
